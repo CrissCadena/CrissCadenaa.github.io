b5285e6d55c714d6220bc40f3ec5a91ec89f7e93
CrissCadena <139088932+CrissCadena@users.noreply.github.com>
2023-07-09 21:31:11 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict"
+
+alert("Buen día usuario, por favor introduce tú nombre")
+alert("Introduce el número del semestre cursado") 
+
+
+var nombre= prompt("Nombre")
+var semestre= prompt ("Semestre")
+
+if (nombre == "Cristina Martínez" & semestre =="4") {
+    document.write ('Buen día'+ nombre)
+    document.write('Usted pude pasar')
+
+}
+   
+
+else{
+    if (nombre == "Yo " & semestre== "4") 
+    document.write('Buen día' + nombre)
+    document.write('Todo ha cargado correctamente')
+}
+
+   { if (nombre == "Tal" & semestre == "4"){
+    document.write("Pase"+ nombre)
+    document.write("Su nombre se ha guardado")} }
+
+  
